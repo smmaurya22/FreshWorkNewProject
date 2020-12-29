09525318dda81f0377fd4a7893f99badffe622d5
Ramesh Maurya <76691743+smmaurya22@users.noreply.github.com>
2020-12-29 14:31:02 +0530

    Update InsertKeyValue.java

--- InsertKeyValue.java
+++ InsertKeyValue.java
@@ -12,7 +12,7 @@ public class InsertKeyValue {
 		String line;
 		try {
 			//file path in our local Database System
-			FileInputStream fin=new FileInputStream("C:\\Users\\Suresh Maurya\\eclipse-workspace\\FreshWork_Project\\output.txt");
+		    FileInputStream fin=new FileInputStream("C:\\Users\\Suresh Maurya\\eclipse-workspace\\FreshWork_Project\\output.txt");
 		    Scanner s=new Scanner(fin);//reading the file using scanner function
 		    while(s.hasNext())//iterating the next line using the while loop 
 		    {
